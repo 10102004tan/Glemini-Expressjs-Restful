ad7c87830d360e1aa55f69b8af1f7a96b341463f
Nguyen Phuong Tan <tannguyen.10102004@gmail.com>
2024-09-28 23:13:14 +0700

    FIX:: error handler

--- src/v1/routes/access/index.js
+++ src/v1/routes/access/index.js
@@ -5,5 +5,6 @@ const { asynHandler } = require('../../auths/utils');
 const accessController = require('../../controllers/access.controller');
 
 router.post('/signup',asynHandler(accessController.signup));
+router.post('/login',asynHandler(accessController.login));
 
 module.exports = router;
--- src/v1/services/access.service.js
+++ src/v1/services/access.service.js
@@ -1,4 +1,5 @@
 'use strict';
+const {BadRequestError} = require('../cores/error.repsone');
 
 class AccessSevice {
     static async signup({email}){
@@ -8,6 +9,18 @@ class AccessSevice {
         return {
             name: "John Doe",
             email: "test@gmail.com"
+        }
+    }
+
+    static async login({email}){
+        //TODO: Implement login service
+        if (email == ""){
+            throw new BadRequestError("Email is required");
+        }
+
+        //const user = await User.find()...
+        return {
+            'name': "John Doe",
         }
     }
 }
