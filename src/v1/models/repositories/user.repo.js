3c9d057864c7f3d70473da3fe0c6a4accb5303fb
Nguyen Phuong Tan <tannguyen.10102004@gmail.com>
2024-10-03 17:55:18 +0700

    FIX:: res + teacher_status when login

--- src/v1/models/repositories/user.repo.js
+++ src/v1/models/repositories/user.repo.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const User = require('../../models/user.model');
+const Teacher = require('../../models/teacher.model');
 
 const findUserByEmail = async (email) => {
-    const foundUser = await User.findOne({ user_email: email });
-    return foundUser;
+    const foundUser = await User.findOne({ user_email: email }).lean();
+    const teacherStatus = await Teacher.findOne({_id:foundUser._id},{teacher_status:1,_id:0}).lean();
+    return (foundUser.user_type="teacher") ? { ...foundUser, teacher_status:teacherStatus.teacher_status} : foundUser;
 };
 
 const findUserById = async (id) => {
@@ -17,8 +19,24 @@ const updatePasswordByEmail = async ({email, password}) => {
     return updated;
 };
 
+const findStatusByUserId= async ({id,type}) => {
+    console.log(id);
+    const statusFound = await User.findOne({_id:id});
+    if(type === 'teacher'){
+       const statusTeacher = await Teacher.findById(id);
+       return {
+              user_status: statusFound.user_status,
+              teacher_status: statusTeacher.teacher_status
+       }
+    }
+    return {
+        user_status: statusFound.user_status
+    }
+};
+
 module.exports = {
     findUserByEmail,
     findUserById,
-    updatePasswordByEmail
+    updatePasswordByEmail,
+    findStatusByUserId
 };
