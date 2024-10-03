b7f1e81732ae4c3a12f696239a68c5ea7ef400af
Datto098 <dat61222@gmail.com>
2024-10-03 22:04:33 +0700

    update create quizz api

--- src/v1/routes/access/index.js
+++ src/v1/routes/access/index.js
@@ -3,29 +3,33 @@ const express = require('express');
 const router = express.Router();
 const { asynHandler } = require('../../auths/utils');
 const accessController = require('../../controllers/access.controller');
-const {authentication} = require('../../auths');
-const {uploadDisk} = require('../../configs/multer.config');
+const { authentication } = require('../../auths');
+const { uploadDisk } = require('../../configs/multer.config');
+const path = require('path');
 
-router.post('/signup',uploadDisk.array('images'),asynHandler(accessController.signup));
-router.post('/login',asynHandler(accessController.login));
+router.post(
+	'/signup',
+	uploadDisk.array('images'),
+	asynHandler(accessController.signup)
+);
+router.post('/login', asynHandler(accessController.login));
+
+
+
 
 /* AUTHENTICATION */
 router.use(asynHandler(authentication));
 /* AUTHENTICATION */
 
-router.post('/me', asynHandler((req, res) => {
-    const user = req.user;
-    res.json(user);
-}))
+router.post(
+	'/me',
+	asynHandler((req, res) => {
+		const user = req.user;
+		res.json(user);
+	})
+);
 router.post('/logout', asynHandler(accessController.logout));
 router.post('/change-password', asynHandler(accessController.changePassword));
 router.post('/refresh-token', asynHandler(accessController.refresh));
-
-
-
-
-
-
-
 
 module.exports = router;
