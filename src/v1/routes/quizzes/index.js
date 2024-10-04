bd97ac01c9ac667c88ce21a159d064a1945806f1
Datto098 <dat61222@gmail.com>
2024-10-04 22:39:57 +0700

    update delete quiz api

--- src/v1/routes/quizzes/index.js
+++ src/v1/routes/quizzes/index.js
@@ -8,5 +8,6 @@ router.post('/create', asynHandler(quizController.createQuiz));
 router.post('/get-by-user', asynHandler(quizController.getQuizByUser));
 router.post('/get-details', asynHandler(quizController.getQuizDetails));
 router.post('/get-questions', asynHandler(quizController.getQuestionsByQuiz));
+router.post('/delete', asynHandler(quizController.deleteQuiz));
 
 module.exports = router;
