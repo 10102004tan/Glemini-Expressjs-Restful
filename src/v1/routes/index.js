eacc69c565481130de9383b857e72b477d55820a
Datto098 <dat61222@gmail.com>
2024-10-06 22:44:23 +0700

    update api update quiz infor

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -8,10 +8,11 @@ const routeAccess = require('./access');
 const routeQuizzes = require('./quizzes');
 const routeQuestions = require('./questions');
 const routeResult = require('./result');
-
+const routeSubjects = require('./subjects');
 router.use('/api/v1/', require('./upload'));
 router.use('/api/v1/quizzes', routeQuizzes);
 router.use('/api/v1/questions', routeQuestions);
+router.use('/api/v1/subjects', routeSubjects);
 router.use('/api/v1/auth', routeAccess);
 router.use('/api/v1/result', routeResult);
 
