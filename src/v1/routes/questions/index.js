ec9cdf971387ae9ac45be9e2295e32c946802620
Nguyen Phuong Tan <tannguyen.10102004@gmail.com>
2024-10-07 13:03:54 +0700

    PUSH:: udpate code

--- src/v1/routes/questions/index.js
+++ src/v1/routes/questions/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const { asynHandler } = require('../../auths/utils');
 const questionController = require('../../controllers/question.controller');
-const upload = require('../../configs/multer.config');
+const {upload} = require('../../configs/multer.config');
 
 router.post('/create', asynHandler(questionController.create));
 router.post('/get', asynHandler(questionController.get));
