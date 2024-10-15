86b59c7e7eada9bc7db413ba965d4b6601dd9fb6
Chau23445 <huynhchau.04014@gmail.com>
2024-10-15 18:25:03 +0700

    up quiz

--- src/v1/controllers/quiz.controller.js
+++ src/v1/controllers/quiz.controller.js
@@ -1,70 +1,70 @@
-'use strict';
-const { CREATED, OK } = require('../cores/success.response');
-const quizService = require('../services/quiz.service');
+"use strict";
+const { CREATED, OK } = require("../cores/success.response");
+const quizService = require("../services/quiz.service");
 
 class QuizController {
-	createQuiz = async (req, res) => {
-		return new CREATED({
-			message: 'Quiz created successfully',
-			metadata: await quizService.createQuiz(req.body),
-		}).send(res);
-	};
+  createQuiz = async (req, res) => {
+    return new CREATED({
+      message: "Quiz created successfully",
+      metadata: await quizService.createQuiz(req.body),
+    }).send(res);
+  };
 
-	getQuizByUser = async (req, res) => {
-		return new OK({
-			message: 'Quiz fetched successfully',
-			metadata: await quizService.getQuizByUser(req.body),
-		}).send(res);
-	};
+  getQuizByUser = async (req, res) => {
+    return new OK({
+      message: "Quiz fetched successfully",
+      metadata: await quizService.getQuizByUser(req.body),
+    }).send(res);
+  };
 
-	getQuizDetails = async (req, res) => {
-		return new OK({
-			message: 'Quiz fetched successfully',
-			metadata: await quizService.getQuizDetails(req.body),
-		}).send(res);
-	};
+  getQuizDetails = async (req, res) => {
+    return new OK({
+      message: "Quiz fetched successfully",
+      metadata: await quizService.getQuizDetails(req.body),
+    }).send(res);
+  };
 
-	getQuestionsByQuiz = async (req, res) => {
-		return new OK({
-			message: 'Questions fetched successfully',
-			metadata: await quizService.getQuestionsByQuiz(req.body),
-		}).send(res);
-	};
+  getQuestionsByQuiz = async (req, res) => {
+    return new OK({
+      message: "Questions fetched successfully",
+      metadata: await quizService.getQuestionsByQuiz(req.body),
+    }).send(res);
+  };
 
-	getQuizzesBySubjectPublished = async (req, res) => {
-		return new OK({
-			message: 'Quizzes fetched successfully',
-			metadata: await quizService.getQuizzesBySubjectIdPublished(req.body),
-		}).send(res);
-	};
+  getQuizzesBySubjectPublished = async (req, res) => {
+    return new OK({
+      message: "Quizzes fetched successfully",
+      metadata: await quizService.getQuizzesBySubjectIdPublished(req.body),
+    }).send(res);
+  };
 
-	deleteQuiz = async (req, res) => {
-		return new OK({
-			message: 'Quiz deleted successfully',
-			metadata: await quizService.deleteQuiz(req.body),
-		}).send(res);
-	};
+  deleteQuiz = async (req, res) => {
+    return new OK({
+      message: "Quiz deleted successfully",
+      metadata: await quizService.deleteQuiz(req.body),
+    }).send(res);
+  };
 
-	updateQuiz = async (req, res) => {
-		return new OK({
-			message: 'Quiz updated successfully',
-			metadata: await quizService.updateQuiz(req.body),
-		}).send(res);
-	};
+  updateQuiz = async (req, res) => {
+    return new OK({
+      message: "Quiz updated successfully",
+      metadata: await quizService.updateQuiz(req.body),
+    }).send(res);
+  };
 
-	uploadQuiz = async (req, res) => {
-		return new OK({
-			message: 'Quiz uploaded successfully',
-			metadata: await quizService.uploadQuiz(req, res),
-		}).send(res);
-	};
+  uploadQuiz = async (req, res) => {
+    return new OK({
+      message: "Quiz uploaded successfully",
+      metadata: await quizService.uploadQuiz(req, res),
+    }).send(res);
+  };
 
-   uploadDoc = async (req, res) => {
-      return new OK({
-         message: 'Document uploaded successfully',
-         metadata: await quizService.uploadDoc(req, res),
-      }).send(res);
-   };
+  uploadDoc = async (req, res) => {
+    return new OK({
+      message: "Document uploaded successfully",
+      metadata: await quizService.uploadDoc(req, res),
+    }).send(res);
+  };
 }
 
 module.exports = new QuizController();
--- src/v1/services/quiz.service.js
+++ src/v1/services/quiz.service.js
@@ -1,207 +1,205 @@
-'use strict';
-const mammoth = require('mammoth');
-const { log } = require('console');
-const { BadRequestError } = require('../cores/error.repsone');
-const questionModel = require('../models/question.model');
-const quizModel = require('../models/quiz.model');
+"use strict";
+const mammoth = require("mammoth");
+const { log } = require("console");
+const { BadRequestError } = require("../cores/error.repsone");
+const questionModel = require("../models/question.model");
+const quizModel = require("../models/quiz.model");
 
 class QuizService {
-	// Hàm tạo quiz
-	async createQuiz({ user_id, quiz_name, quiz_description }) {
-		const newQuiz = await quizModel.create({
-			user_id,
-			quiz_name,
-			quiz_description,
-		});
+  // Hàm tạo quiz
+  async createQuiz({ user_id, quiz_name, quiz_description }) {
+    const newQuiz = await quizModel.create({
+      user_id,
+      quiz_name,
+      quiz_description,
+    });
 
-		if (!newQuiz) {
-			throw new BadRequestError('Quiz not created');
-		}
-		return newQuiz;
-	}
+    if (!newQuiz) {
+      throw new BadRequestError("Quiz not created");
+    }
+    return newQuiz;
+  }
 
-	// Hàm lấy danh sách quiz theo user
-	async getQuizByUser({ user_id }) {
-		console.log(user_id);
-		const quizzies = await quizModel.find({ user_id });
-		if (!quizzies) {
-			throw new BadRequestError('Quiz not found');
-		}
+  // Hàm lấy danh sách quiz theo user
+  async getQuizByUser({ user_id }) {
+    console.log(user_id);
+    const quizzies = await quizModel.find({ user_id });
+    if (!quizzies) {
+      throw new BadRequestError("Quiz not found");
+    }
 
-		return quizzies;
-	}
+    return quizzies;
+  }
 
-	// Hàm lấy thông tin quiz theo id
-	async getQuizById({ quiz_id }) {
-		const quiz = await quizModel.findById(quiz_id);
-		if (!quiz) {
-			throw new BadRequestError('Quiz not found');
-		}
-		return quiz;
-	}
+  // Hàm lấy thông tin quiz theo id
+  async getQuizById({ quiz_id }) {
+    const quiz = await quizModel.findById(quiz_id);
+    if (!quiz) {
+      throw new BadRequestError("Quiz not found");
+    }
+    return quiz;
+  }
 
-	// Hàm lấy danh sách câu hỏi theo quiz
-	async getQuestionsByQuiz({ quiz_id }) {
-		const questions = await questionModel
-			.find({ quiz_id })
-			.populate('question_answer_ids')
-			.populate('correct_answer_ids')
-			.exec(); 
+  // Hàm lấy danh sách câu hỏi theo quiz
+  async getQuestionsByQuiz({ quiz_id }) {
+    const questions = await questionModel
+      .find({ quiz_id })
+      .populate("question_answer_ids")
+      .populate("correct_answer_ids")
+      .exec();
 
-		if (!questions || questions.length === 0) {
-			throw new BadRequestError('Questions not found');
-		}
+    if (!questions || questions.length === 0) {
+      throw new BadRequestError("Questions not found");
+    }
 
-		return questions;
-	}
+    return questions;
+  }
 
-	async getQuizzesBySubjectIdPublished({subjectId}) {		
-		let query = {};
-		if (subjectId) {
-			try {
-				query.subject_ids = { $in: [subjectId] };
-			} catch (error) {
-				throw new BadRequestError('Invalid subject ID format');
-			}
-		}
+  async getQuizzesBySubjectIdPublished({ subjectId }) {
+    let query = {};
+    if (subjectId) {
+      try {
+        query.subject_ids = { $in: [subjectId] };
+      } catch (error) {
+        throw new BadRequestError("Invalid subject ID format");
+      }
+    }
 
-		let quizzes = await quizModel.find(query).populate("user_id");
+    let quizzes = await quizModel.find(query).populate("user_id");
 
-		if (subjectId === null) {
-			quizzes = await quizModel.find({});
-		}
+    if (subjectId === null) {
+      quizzes = await quizModel.find({});
+    }
 
-		const publishedQuizzes = quizzes.filter(quiz => quiz.quiz_status === 'published');
+    const publishedQuizzes = quizzes.filter(
+      (quiz) => quiz.quiz_status === "published"
+    );
 
-		if (publishedQuizzes.length === 0) {
-			throw new BadRequestError('No published quizzes found');
-		}
+    if (publishedQuizzes.length === 0) {
+      throw new BadRequestError("No published quizzes found");
+    }
 
-		return publishedQuizzes;
-	}
+    return publishedQuizzes;
+  }
 
+  // Hàm lấy thông tin chi tiết của quiz
+  async getQuizDetails({ quiz_id }) {
+    console.log(quiz_id);
+    const quiz = await quizModel.findById(quiz_id);
+    if (!quiz) {
+      throw new BadRequestError("Quiz not found");
+    }
 
+    return quiz;
+  }
 
+  // Hàm xóa quiz
+  async deleteQuiz({ quiz_id }) {
+    const quiz = await quizModel.findByIdAndDelete(quiz_id);
+    if (!quiz) {
+      throw new BadRequestError("Quiz not found");
+    }
+    return quiz;
+  }
 
+  // Hàm cập nhật thông tin quiz
+  async updateQuiz({
+    quiz_id,
+    quiz_name,
+    quiz_description,
+    quiz_subjects,
+    quiz_status,
+    quiz_thumb,
+  }) {
+    const quiz = await quizModel.findByIdAndUpdate(
+      quiz_id,
+      {
+        quiz_name,
+        quiz_description,
+        quiz_thumb,
+      },
+      { new: true }
+    );
 
-	// Hàm lấy thông tin chi tiết của quiz
-	async getQuizDetails({ quiz_id }) {
-		console.log(quiz_id);
-		const quiz = await quizModel.findById(quiz_id);
-		if (!quiz) {
-			throw new BadRequestError('Quiz not found');
-		}
+    if (!quiz) {
+      throw new BadRequestError("Quiz not found");
+    }
 
-		return quiz;
-	}
+    // Cập nhật thông tin cho trường subject_ids
+    if (quiz_subjects && quiz_subjects.length > 0) {
+      quiz.subject_ids = quiz_subjects;
+      await quiz.save();
+    }
 
-	// Hàm xóa quiz
-	async deleteQuiz({ quiz_id }) {
-		const quiz = await quizModel.findByIdAndDelete(quiz_id);
-		if (!quiz) {
-			throw new BadRequestError('Quiz not found');
-		}
-		return quiz;
-	}
+    // Cập nhật thông tin cho trường quiz_status
+    if (quiz_status) {
+      console.log(quiz_status);
+      switch (quiz_status) {
+        case "published":
+          quiz.quiz_status = "published";
+          break;
+        case "unpublished":
+          quiz.quiz_status = "unpublished";
+          break;
+        case "deleted":
+          quiz.quiz_status = "deleted";
+          break;
+        default:
+          break;
+      }
+      await quiz.save();
+    }
 
-	// Hàm cập nhật thông tin quiz
-	async updateQuiz({
-		quiz_id,
-		quiz_name,
-		quiz_description,
-		quiz_subjects,
-		quiz_status,
-		quiz_thumb,
-	}) {
-		const quiz = await quizModel.findByIdAndUpdate(
-			quiz_id,
-			{
-				quiz_name,
-				quiz_description,
-				quiz_thumb,
-			},
-			{ new: true }
-		);
+    return quiz;
+  }
 
-		if (!quiz) {
-			throw new BadRequestError('Quiz not found');
-		}
+  // Hàm upload file ảnh
+  async uploadQuiz(req, res) {
+    if (!req.file) {
+      throw new BadRequestError("No file uploaded");
+    }
+    const imageUrl = `http://192.168.0.19:8000/api/v1/uploads/quizzes/${req.file.filename}`;
+    return imageUrl;
+  }
 
-		// Cập nhật thông tin cho trường subject_ids
-		if (quiz_subjects && quiz_subjects.length > 0) {
-			quiz.subject_ids = quiz_subjects;
-			await quiz.save();
-		}
+  // Hàm upload file docx
+  async uploadDoc(req, res) {
+    // console.log(req.file);
+    if (!req.file) {
+      throw new BadRequestError("No file uploaded");
+    }
 
-		// Cập nhật thông tin cho trường quiz_status
-		if (quiz_status) {
-			console.log(quiz_status);
-			switch (quiz_status) {
-				case 'published':
-					quiz.quiz_status = 'published';
-					break;
-				case 'unpublished':
-					quiz.quiz_status = 'unpublished';
-					break;
-				case 'deleted':
-					quiz.quiz_status = 'deleted';
-					break;
-				default:
-					break;
-			}
-			await quiz.save();
-		}
+    const filePath = req.file.path;
+    const result = await mammoth.extractRawText({ path: filePath });
+    const questions = QuizService.parseQuestionsFromText(result.value);
+    return questions;
+  }
 
-		return quiz;
-	}
+  // Hàm phân tích nội dung và tạo câu hỏi từ file docx
+  static parseQuestionsFromText = (text) => {
+    const lines = text.split("\n");
+    const questions = [];
+    let currentQuestion = null;
 
-	// Hàm upload file ảnh
-	async uploadQuiz(req, res) {
-		if (!req.file) {
-			throw new BadRequestError('No file uploaded');
-		}
-		const imageUrl = `http://192.168.1.8:8000/api/v1/uploads/quizzes/${req.file.filename}`;
-		return imageUrl;
-	}
+    lines.forEach((line) => {
+      if (line.startsWith("Question")) {
+        if (currentQuestion) {
+          questions.push(currentQuestion);
+        }
+        currentQuestion = { question: line, answers: [] };
+      } else if (line.match(/^[A-Z]\./)) {
+        currentQuestion.answers.push(line);
+      } else if (line.startsWith("Correct Answer:")) {
+        currentQuestion.correctAnswer = line.split(": ")[1];
+      }
+    });
 
-	// Hàm upload file docx
-	async uploadDoc(req, res) {
-		// console.log(req.file);
-		if (!req.file) {
-			throw new BadRequestError('No file uploaded');
-		}
+    if (currentQuestion) {
+      questions.push(currentQuestion);
+    }
 
-		const filePath = req.file.path;
-		const result = await mammoth.extractRawText({ path: filePath });
-		const questions = QuizService.parseQuestionsFromText(result.value);
-		return questions;
-	}
-
-	// Hàm phân tích nội dung và tạo câu hỏi từ file docx
-	static parseQuestionsFromText = (text) => {
-		const lines = text.split('\n');
-		const questions = [];
-		let currentQuestion = null;
-
-		lines.forEach((line) => {
-			if (line.startsWith('Question')) {
-				if (currentQuestion) {
-					questions.push(currentQuestion);
-				}
-				currentQuestion = { question: line, answers: [] };
-			} else if (line.match(/^[A-Z]\./)) {
-				currentQuestion.answers.push(line);
-			} else if (line.startsWith('Correct Answer:')) {
-				currentQuestion.correctAnswer = line.split(': ')[1];
-			}
-		});
-
-		if (currentQuestion) {
-			questions.push(currentQuestion);
-		}
-
-		return questions;
-	};
+    return questions;
+  };
 }
 
 module.exports = new QuizService();
