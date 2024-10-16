adb5f52d643e733e6b87957cb991654d3fd5aef4
ttchien <chientt.00.student@fit.tdc.edu.vn>
2024-10-16 15:40:00 +0700

    UPDATE:: update data for banner and fix UI something

--- src/v1/routes/result/index.js
+++ src/v1/routes/result/index.js
@@ -1,8 +1,13 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const { authentication } = require('../../auths');
 const { asynHandler } = require('../../auths/utils');
 const resultController = require('../../controllers/result.controller');
+
+/* AUTHENTICATION */
+router.use(asynHandler(authentication));
+/* AUTHENTICATION */
 
 router.post('/save-question', resultController.saveQuestion);
 router.post('/complete-quiz', resultController.completeQuiz);
