3639594aa92d2d96ce77ffe6b14625562543b168
Chau23445 <huynhchau.04014@gmail.com>
2024-10-16 16:43:32 +0700

    update be

--- src/v1/routes/quizzes/index.js
+++ src/v1/routes/quizzes/index.js
@@ -1,28 +1,28 @@
-'use strict';
-const express = require('express');
+"use strict";
+const express = require("express");
 const router = express.Router();
-const { asynHandler } = require('../../auths/utils');
-const quizController = require('../../controllers/quiz.controller');
-const { uploadQuizzes, uploadDocs } = require('../../configs/multer.config');
-const mammoth = require('mammoth');
+const { asynHandler } = require("../../auths/utils");
+const quizController = require("../../controllers/quiz.controller");
+const { uploadQuizzes, uploadDocs } = require("../../configs/multer.config");
+const mammoth = require("mammoth");
 
-router.post('/create', asynHandler(quizController.createQuiz));
-router.post('/delete', asynHandler(quizController.deleteQuiz));
-router.post('/update', asynHandler(quizController.updateQuiz));
-router.post('/get-by-user', asynHandler(quizController.getQuizByUser));
-router.post('/get-details', asynHandler(quizController.getQuizDetails));
-router.post('/get-questions', asynHandler(quizController.getQuestionsByQuiz));
+router.post("/create", asynHandler(quizController.createQuiz));
+router.post("/delete", asynHandler(quizController.deleteQuiz));
+router.post("/update", asynHandler(quizController.updateQuiz));
+router.post("/get-by-user", asynHandler(quizController.getQuizByUser));
+router.post("/get-details", asynHandler(quizController.getQuizDetails));
+router.post("/get-questions", asynHandler(quizController.getQuestionsByQuiz));
 
 router.post(
-	'/upload',
-	uploadQuizzes.single('quiz_image'),
-	asynHandler(quizController.uploadQuiz)
+  "/upload",
+  uploadQuizzes.single("quiz_image"),
+  asynHandler(quizController.uploadQuiz)
 );
 
 router.post(
-	'/docs/upload',
-	uploadDocs.single('file'),
-	asynHandler(quizController.uploadDoc)
+  "/docs/upload",
+  uploadDocs.single("file"),
+  asynHandler(quizController.uploadDoc)
 );
 
 module.exports = router;
